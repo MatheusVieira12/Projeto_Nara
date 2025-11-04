fa1a26c2474faba80ea8b3e1be6002ced63c3a1b
MatheusVieira12 <matheuscoimbra21@outlook.com>
2025-11-04 16:04:19 -0300

    path mais simples

--- correcao_hora.py
+++ correcao_hora.py
@@ -1,13 +1,12 @@
 import pandas as pd
 
-path = r"C:\Users\PC\Desktop\projetos\Projeto_Nara\NARA_csv"
+# Lê o arquivo dentro da pasta NARA_csv
+df = pd.read_csv("NARA_csv/venda 1.csv")
 
-df = pd.read_csv(fr"{path}\venda 1.csv")
-
-# Converte a coluna data para datetime e remove a parte da hora
+# Remove a parte da hora
 df['Data'] = pd.to_datetime(df['Data']).dt.date
 
-novo_arquivo = fr"{path}\venda_1_semH.csv"
-df.to_csv(novo_arquivo, index=False)
+# Salva o novo arquivo na mesma pasta
+df.to_csv("NARA_csv/vendas_1_semH.csv", index=False)
 
-print(f"✅ Novo arquivo criado com sucesso: {novo_arquivo}")
+print("✅ Novo arquivo criado em 'NARA_csv/vendass_1_semH.csv'")
--- leftJoin_produtos.py
+++ leftJoin_produtos.py
@@ -1,27 +1,22 @@
 import pandas as pd
 
-# Caminho da pasta
-path = r"C:\Users\PC\Desktop\projetos\Projeto_Nara\NARA_csv"
+# Ler os arquivos diretamente com o caminho completo
+produtos = pd.read_csv("NARA_csv/produtos.csv", sep=",")
+corrigidos = pd.read_csv("NARA_csv/produtos_sem_marca_corrigidos.csv", sep=",")
 
-# Ler os arquivos
-produtos = pd.read_csv(fr"{path}\produtos.csv")
-corrigidos = pd.read_csv(fr"{path}\produtos_sem_marca_corrigidos.csv")
-
-
-df = produtos.merge(
-    corrigidos[["ID_Produto", "Marca"]],
-    on="ID_Produto",
+# Realizar a concatenação com merge
+df = produtos.merge(corrigidos[["ID_Produto", "Marca"]], on="ID_Produto",
     how="left",
     suffixes=("", "_corrigida")
 )
 
-# Preencher valores ausentes em 'Marca' com a 'Marca_corrigida'
+# Preencher valores ausentes na coluna 'Marca' com os valores corrigidos
 df["Marca"] = df["Marca"].fillna(df["Marca_corrigida"])
 
-# Remover a coluna auxiliar
+# Remover a coluna auxiliar 'Marca_corrigida'
 df = df.drop(columns=["Marca_corrigida"])
 
-# Salvar o resultado
-df.to_csv(fr"{path}\produtos_atualizado.csv", index=False, encoding="utf-8")
+# Salvar o arquivo final com as alterações
+df.to_csv("NARA_csv/produtos_atualizado.csv", index=False, encoding="utf-8")
 
 print("✅ Arquivo 'produtos_atualizado.csv' criado com sucesso!")
